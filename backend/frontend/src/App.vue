705f4e0507761256e22a70350aded06472263306
Andrew David <56502440+andydavidlee@users.noreply.github.com>
2021-03-17 14:29:49 +1100

    Add files via upload

---
+++ backend/frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <app-header/>
+   <router-view />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/Header.vue'
+
+export default {
+
+
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
